90fa3cb0072b300a615d0fc4c8e6ab773f7dc451
mateuszbieniek <mbieniek@autocentrum.pl>
2019-11-21 08:23:11 +0100

    - added sass, bootstrap and division into components

---
+++ src/components/Menu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SubMenu from "./SubMenu";
+
+export default class Menu extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                Menu
+                <SubMenu/>
+            </div>
+        )
+    }
+}
---
+++ src/components/SubMenu.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class SubMenu extends React.Component {
+
+
+    render () {
+        return (
+            <div> SubMenu </div>
+        )
+    }
+}
