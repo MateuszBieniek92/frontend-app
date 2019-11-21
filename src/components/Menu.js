5fb815de0df928352fc0b951bfa89a554ff64a37
mateuszbieniek <mbieniek@autocentrum.pl>
2019-11-21 21:07:10 +0100

    - left menu progress

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import SubMenu from "./SubMenu";
+import './menu.scss';
+
+import {Tab, Nav} from 'react-bootstrap';
 
 export default class Menu extends React.Component {
 
@@ -7,7 +10,8 @@ export default class Menu extends React.Component {
         super();
 
         this.state = {
-            menuItems: []
+            menuItems: [],
+            showMenu: false
         }
     }
 
@@ -32,17 +36,39 @@ export default class Menu extends React.Component {
             .catch(error => console.log('Błąd ładowania: ' + error));
     };
 
+    showSubMenu = () => {
+        this.setState({
+            showMenu: false,
+        })
+    }
+
     render() {
         return (
-            <header>
-                {this.state.menuItems.map((menuItem) => (
-                    <div>
-                        <ul>
-                            <li>{menuItem.name}</li>
-                        </ul>
-                        <SubMenu subMenu={menuItem.subMenu}/>
+            <header className="menu">
+                <Tab.Container>
+                    <div className="menu__sidebar">
+                        <Nav className="flex-column">
+                            {this.state.menuItems.map((data) => (
+                                <Nav.Item key={data.id}>
+                                    <Nav.Link eventKey={data.id} onClick={() => {
+                                        this.setState({showMenu: true});
+                                    }}>{data.name}</Nav.Link>
+                                </Nav.Item>
+                            ))}
+                        </Nav>
                     </div>
-                ))}
+                    <div className="menu__content">
+                        {this.state.showMenu ? (
+                            <Tab.Content>
+                                {this.state.menuItems.map((data) => (
+                                    <Tab.Pane key={data.id} eventKey={data.id}>
+                                        <SubMenu showSubMenu={this.showSubMenu} subMenu={data.subMenu}/>
+                                    </Tab.Pane>
+                                ))}
+                            </Tab.Content>
+                        ) : ''}
+                    </div>
+                </Tab.Container>
             </header>
         )
     }
