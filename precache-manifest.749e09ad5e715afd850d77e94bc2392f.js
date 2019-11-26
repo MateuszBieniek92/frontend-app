ff477e98fb1c12e13efb481de5f6afe8afc3dd35
mateuszbieniek <mbieniek@autocentrum.pl>
2019-11-26 09:04:35 +0100

    Updates

---
+++ precache-manifest.749e09ad5e715afd850d77e94bc2392f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b474a705343529baf6013e544ecdefad",
+    "url": "/frontend-app/index.html"
+  },
+  {
+    "revision": "79469b56fda44471d951",
+    "url": "/frontend-app/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "24f0c981ec25be17f68b",
+    "url": "/frontend-app/static/css/main.56bbbd2e.chunk.css"
+  },
+  {
+    "revision": "79469b56fda44471d951",
+    "url": "/frontend-app/static/js/2.b3bce070.chunk.js"
+  },
+  {
+    "revision": "24f0c981ec25be17f68b",
+    "url": "/frontend-app/static/js/main.9de1ae3a.chunk.js"
+  },
+  {
+    "revision": "0e6b6d36bdcd941e3e18",
+    "url": "/frontend-app/static/js/runtime-main.25cbc7b2.js"
+  },
+  {
+    "revision": "45f89513452cb10c35d6cf05c4dbd85e",
+    "url": "/frontend-app/static/media/example-svg.45f89513.svg"
+  },
+  {
+    "revision": "b1bdfe99b8fdbb805cb0990b018b6b24",
+    "url": "/frontend-app/static/media/sample-image.b1bdfe99.png"
+  }
+]);
